a7106c3b65e44b4cf8247e7a52b9cf5bccb865f6
Rodolfo <rodolfo.jaramillo@outlook.com>
2023-02-01 14:20:38 -0700

    Challenge del Encriptador Alura: Iniciado

---
+++ script.js
@@ -0,0 +1,51 @@
+function encriptar(){
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/e/img,"enter");
+    var textoEncriptado = textoEncriptado.replace(/o/img,"ober");
+    var textoEncriptado = textoEncriptado.replace(/i/img,"imes");
+    var textoEncriptado = textoEncriptado.replace(/a/img,"ai");
+    var textoEncriptado = textoEncriptado.replace(/u/img,"ufat");
+
+
+
+    document.getElementById("imagen").style.display="none";
+    document.getElementById("subtitulo").style.display="none";
+    document.getElementById("textoDesencriptado").style.display="show";
+    document.getElementById("textoDesencriptado").style.display="inherit";
+
+
+    document.getElementById("textoDesencriptado").innerHTML=textoEncriptado;
+
+    document.getElementById("botonCopiar").style.display="show";
+    document.getElementById("botonCopiar").style.display="inherit";
+
+
+    
+
+}
+function desencriptar(){
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/enter/img,"e");
+    var textoEncriptado = textoEncriptado.replace(/ober/img,"o");
+    var textoEncriptado = textoEncriptado.replace(/imes/img,"i");
+    var textoEncriptado = textoEncriptado.replace(/ai/img,"a");
+    var textoEncriptado = textoEncriptado.replace(/ufat/img,"u");
+
+    document.getElementById("imagen").style.display="none";
+    document.getElementById("subtitulo").style.display="none";
+    document.getElementById("textoDesencriptado").style.display="show";
+    document.getElementById("textoDesencriptado").style.display="inherit";
+
+    document.getElementById("textoDesencriptado").innerHTML=textoEncriptado;
+
+    document.getElementById("botonCopiar").style.display="show";
+    document.getElementById("botonCopiar").style.display="inherit";
+}
+
+function copiar(){
+    var contenido=document.querySelector("#textoDesencriptado")
+    contenido.select();
+    document.execCommand("copy");
+}
